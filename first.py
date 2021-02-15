a1c578faa0fde7c6b7ce724dd6b943e04a457b95
Bibekbahadurkc <bibekbahadurkc@gmail.com>
2021-02-15 21:44:26 +0545

    Initial commit

---
+++ first.py
@@ -0,0 +1,5 @@
+#program to calculate the area of rectangle
+length=int(input('Enter the length:'))
+breadth=int(input('Enter the breadth:'))
+area=length * breadth
+print('the area of rectangle is ',area)
