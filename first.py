0f546883babfef590548d0d46bea59ccd01293a4
Bibekbahadurkc <bibekbahadurkc@gmail.com>
2021-02-17 11:20:12 +0545

    New file

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
 #program to calculate the area of rectangle
-length=int(input('Enter the length:'))
-breadth=int(input('Enter the breadth:'))
+length=2
+breadth=3
 area=length * breadth
-print('the area of rectangle is ',area)
+print('the area of rectangle is '+str(area))
+print('the area of rectangle is',area)
+print(f'the area of rectangle is {area}')
---
+++ venv/circle.py
@@ -0,0 +1,5 @@
+# program to calculate the are of circle
+radius=float(input('Enter the radius: '))
+pi=3.14
+area=pi*radius*radius**2
+print('The area of circle is',area)
